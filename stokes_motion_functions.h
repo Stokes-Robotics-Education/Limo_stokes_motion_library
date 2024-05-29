6cb0fb6b763242b5beb312467cbcc03932d420a1
dlgst <139267983+dlgst@users.noreply.github.com>
2024-05-29 13:53:55 -0500

    Delete stokes_motion_functions.h

--- stokes_motion_functions.h
+++
@@ -1,8 +0,0 @@
-#ifndef STOKES_MOTION_FUNCTIONS_H
-#define STOKES_MOTION_FUNCTIONS_H
-
-void Drive(float velocity, float time);
-void Strafe(float velocity, float time);
-void Turn(float velocity, float time);
-
-#endif
