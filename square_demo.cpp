bcc20e69b0995d02192209c2f1b2144b70230662
dlgst <139267983+dlgst@users.noreply.github.com>
2024-05-29 13:45:26 -0500

    Add files via upload

---
+++ square_demo.cpp
@@ -0,0 +1,27 @@
+#include <sstream>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <geometry_msgs/Twist.h>
+#include "../include/stokes_motion_functions.h"
+
+
+
+int main(int argc, char **argv)
+{
+    	// The ROS node is initialized
+    	ros::init(argc, argv, "square_demo");
+
+
+	Drive(50, 1);
+	Strafe(50, 1);
+	Drive(-50, 1);
+	Strafe(-50, 1);
+	//Turn(float velocity(percent), float time(seconds));
+	//Strafe(float velocity(percent), float time(seconds));
+
+//Type code in here using the functions above
+
+	return 0;
+
+}
+
