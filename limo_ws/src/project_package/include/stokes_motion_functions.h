06ba3af8bcd95a6ccc86d54b0752f9d9e5c9343a
dlgst <139267983+dlgst@users.noreply.github.com>
2024-05-29 13:51:40 -0500

    Add files via upload

---
+++ limo_ws/src/project_package/include/stokes_motion_functions.h
@@ -0,0 +1,8 @@
+#ifndef STOKES_MOTION_FUNCTIONS_H
+#define STOKES_MOTION_FUNCTIONS_H
+
+void Drive(float velocity, float time);
+void Strafe(float velocity, float time);
+void Turn(float velocity, float time);
+
+#endif
